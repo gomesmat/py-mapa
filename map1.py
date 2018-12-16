37acce70f9f1f4800a03cef28cba644d0beaf115
Matheus Gomes <matheusribeirogomes@hotmail.com>
2018-12-16 03:14:42 -0200

    first commit

---
+++ map1.py
@@ -0,0 +1,29 @@
+import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+nome = list(data["NAME"])
+el = list(data["ELEV"])
+
+def colorir(eleva):
+    if (eleva<2000):
+        return 'green'
+    elif 2000 <= eleva < 3000:
+        return 'yellow'
+    elif (eleva >= 3000 and eleva < 4000):
+        return 'orange'
+    else:
+        return 'red'
+
+map = folium.Map(location=[40.629902, -120.831001], tiles="Mapbox Bright")
+
+
+fg=folium.FeatureGroup(name="My Map")
+
+for lt, ln, nm, el in zip(lat, lon, nome, el):
+    fg.add_child(folium.Marker(location=[lt, ln], popup=nm, icon=folium.Icon(color=colorir(el))))
+map.add_child(fg)
+
+map.save("Map1.html")
