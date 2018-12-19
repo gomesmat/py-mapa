171da363c1bb6ae0827237a5948321dc1dd96b64
Matheus Gomes <matheusribeirogomes@hotmail.com>
2018-12-18 23:43:40 -0200

    3

--- map3.py
+++ map3.py
@@ -17,13 +17,15 @@ def colorir(eleva):
     else:
         return 'red'
 
-map = folium.Map(location=[40.629902, -120.831001], tiles="Mapbox Bright")
+map = folium.Map(location=[40.629902, -120.831001], zoom_start=6, tiles="Mapbox Bright")
 
 
 fg=folium.FeatureGroup(name="My Map")
 
 for lt, ln, nm, el in zip(lat, lon, nome, el):
     fg.add_child(folium.CircleMarker(location=[lt, ln], radius=7, popup=nm + " m", fill_color=colorir(el), color='grey', fill_opacity=0.7))
-map.add_child(fg)
 
-map.save("Map1.html")
+fg.add_child(folium.GeoJson(data=(open('world.json', 'r', encoding='utf-8-sig').read())))
+
+map.add_child(fg)
+map.save("Map3.html")
